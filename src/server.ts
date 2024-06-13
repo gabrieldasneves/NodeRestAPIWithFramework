36d5c4647f9b4e146972c5c004cf1e0a279e93f5
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-06-13 15:14:53 +0900

    feat: install and setup knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
+import { knex } from "./database";
 
 const app = fastify();
 
-app.get("/hello", () => {
-  return "ola mundo";
+app.get("/hello", async () => {
+  const test = await knex("sqlite_schema").select("*");
+  return test;
 });
 
 app.listen({ port: 3333 }).then(() => {
