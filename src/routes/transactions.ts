a203b93d5329ddb705199e99b0ae7bf2f89cde01
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-06-18 16:44:25 +0900

    fix: first fastify plugin

---
+++ src/routes/transactions.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from "fastify";
+import { knex } from "../database";
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/hello", async () => {
+    const transaction = await knex("transactions")
+      .insert({
+        id: crypto.randomUUID(),
+        title: "testtransaction",
+        amount: 1000,
+      })
+      .returning("*");
+    return transaction;
+  });
+}
