db0161498ef3c4e05d6c2fa8a2c4a5c016921fa5
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-06-18 16:25:08 +0900

    feat: use zod

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,11 @@
 import { knex as setupKnex, Knex } from "knex";
 import "dotenv/config";
-import { error } from "console";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("");
-}
+import { env } from "./env";
 
 export const config: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
