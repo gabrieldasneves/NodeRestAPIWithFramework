6adced94d47e7330e5dbfce5bb9a6b89c75cb4f6
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-06-21 17:17:24 +0900

    feat: uses cookies with fastify

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import { knex } from "./database";
 import crypto from "node:crypto";
 import { env } from "./env";
 import { transactionsRoutes } from "./routes/transactions";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
-
+app.register(cookie);
 app.register(transactionsRoutes);
 
 app.listen({ port: env.PORT }).then(() => {
