adac18482780ff67916445221b2e13d0c20b0815
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-06-24 18:24:43 +0900

    feat: test for transactions creation

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,5 @@
-import fastify from "fastify";
-import { knex } from "./database";
-import crypto from "node:crypto";
+import { app } from "./app";
 import { env } from "./env";
-import { transactionsRoutes } from "./routes/transactions";
-import cookie from "@fastify/cookie";
-
-const app = fastify();
-app.register(cookie);
-app.register(transactionsRoutes);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP server running");
--- test/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-
-test("user can create new transaction", () => {
-  const responseStatusCode = 201;
-
-  expect(responseStatusCode).toEqual(201);
-});
