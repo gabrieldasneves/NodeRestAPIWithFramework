76cd36afa4e8795b0c97f7599c5430b82c76c7a8
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-06-24 18:27:59 +0900

    feat: creating context using describe

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,21 +1,23 @@
-import { expect, test, beforeAll, afterAll } from "vitest";
+import { expect, test, beforeAll, afterAll, describe } from "vitest";
 import request from "supertest";
 import { app } from "../src/app";
 
-beforeAll(async () => {
-  await app.ready();
-});
-
-afterAll(async () => {
-  await app.close();
-});
-
-test("user can create new transaction", async () => {
-  const response = await request(app.server).post("/transactions").send({
-    title: "new transaction",
-    amount: 4044,
-    type: "credit",
+describe("Transactions routes", () => {
+  beforeAll(async () => {
+    await app.ready();
   });
 
-  expect(response.statusCode).toEqual(201);
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test("user can create new transaction", async () => {
+    const response = await request(app.server).post("/transactions").send({
+      title: "new transaction",
+      amount: 4044,
+      type: "credit",
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
 });
